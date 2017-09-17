2fc3f7f64649263faffa6d83114821b451236a8a
N Space <nspace-seattlevr@outlook.com>
2017-09-16 19:58:43 -0700

    adding check to only display 2 controller

--- Assets/Scripts/DisplacmentRotation.cs
+++ Assets/Scripts/DisplacmentRotation.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class DisplacmentRotation : MonoBehaviour {
 
     public Transform displacementTransform;
+    public Vector3 displacement;
     public Vector3 direction;
    
 
     public Vector3 GetTargetPosition() {
            
         Vector3 targetPostition = new Vector3(
-            (direction.x != 0) ? direction.x * displacementTransform.position.x : transform.position.x,
-            (direction.y != 0) ? direction.y * displacementTransform.position.y : transform.position.y,
-        (direction.z != 0) ?  direction.z* displacementTransform.position.z : transform.position.z
+            (direction.x != 0) ? direction.x * displacementTransform.position.x + displacement.x : transform.position.x,
+            (direction.y != 0) ? direction.y * displacementTransform.position.y + displacement.y : transform.position.y,
+        (direction.z != 0) ?  direction.z* displacementTransform.position.z + displacement.z : transform.position.z
                                      );
         return targetPostition;
     }
