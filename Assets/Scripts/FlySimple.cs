94d111dc9cd50de46e999cb57d88a18069be9502
mjstrwy <ralleenwillems@gmail.com>
2017-09-16 23:22:17 -0700

    Added damping to ship controls (might want to remove this if it causes issues in VR), asteroids, laser, laser sound, and ability to destroy objects tagged "Enemy."

--- Assets/Scripts/FlySimple.cs
+++ Assets/Scripts/FlySimple.cs
@@ -6,11 +6,15 @@ public class FlySimple : MonoBehaviour {
 
 	public float speed = 50.0f;
 	public float rotateSpeed = 25.0f;
+	public float damp = 1;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+	float _rotationAmount;
+	float _rotationAmountVelocity;
 
 	void FixedUpdate () {
 		float transAmount = speed * Time.deltaTime;
@@ -19,27 +23,66 @@ public class FlySimple : MonoBehaviour {
 		//Keyboard controls
 		if(Input.GetKey("up")){
 			//pitch ship down by rotateAmount each frame
-			transform.Rotate(rotateAmount, 0, 0);
+			//transform.Rotate(rotateAmount, 0, 0);
+			_rotationAmount = Mathf.SmoothDamp(_rotationAmount, rotateAmount, ref _rotationAmountVelocity, damp);
+			transform.Rotate(_rotationAmount, 0, 0);
+		} else {
+			_rotationAmount = Mathf.SmoothDamp(_rotationAmount, 0, ref _rotationAmountVelocity, damp);
+			transform.Rotate(_rotationAmount, 0, 0);
 		}
+
+
 		if(Input.GetKey("down")){
 			//pitch ship up by rotateAmount each frame
-			transform.Rotate(-rotateAmount, 0, 0);
+			//transform.Rotate(-rotateAmount, 0, 0);
+			_rotationAmount = Mathf.SmoothDamp(_rotationAmount, -rotateAmount, ref _rotationAmountVelocity, damp);
+			transform.Rotate(_rotationAmount, 0, 0);
+		} else {
+			_rotationAmount = Mathf.SmoothDamp(_rotationAmount, 0, ref _rotationAmountVelocity, damp);
+			transform.Rotate(_rotationAmount, 0, 0);
 		}
+
+
 		if(Input.GetKey("left")){
 			//yaw ship left by rotateAmount each frame
-			transform.Rotate(0, -rotateAmount, 0);
+			_rotationAmount = Mathf.SmoothDamp(_rotationAmount, -rotateAmount, ref _rotationAmountVelocity, damp);
+			//transform.Rotate(0, -rotationAmount, 0);
+			transform.Rotate(0, _rotationAmount, 0);
+		} else {
+			_rotationAmount = Mathf.SmoothDamp(_rotationAmount, 0, ref _rotationAmountVelocity, damp);
+			transform.Rotate(0, _rotationAmount, 0);
 		}
+
+
 		if(Input.GetKey("right")){
 			//yaw ship right by rotateAmount each frame
-			transform.Rotate(0, rotateAmount, 0);
+			//transform.Rotate(0, rotateAmount, 0);
+			_rotationAmount = Mathf.SmoothDamp(_rotationAmount, rotateAmount, ref _rotationAmountVelocity, damp);
+			transform.Rotate(0, _rotationAmount, 0);
+		} else {
+			_rotationAmount = Mathf.SmoothDamp(_rotationAmount, 0, ref _rotationAmountVelocity, damp);
+			transform.Rotate(0, _rotationAmount, 0);
 		}
+
 		if(Input.GetKey("a")){
 			//roll ship  left by rotateAmount each frame
-			transform.Rotate(0, 0, rotateAmount);
+			//transform.Rotate(0, 0, rotateAmount);
+			_rotationAmount = Mathf.SmoothDamp(_rotationAmount, rotateAmount, ref _rotationAmountVelocity, damp);
+			transform.Rotate(0, 0, _rotationAmount);
+		} else {
+			_rotationAmount = Mathf.SmoothDamp(_rotationAmount, 0, ref _rotationAmountVelocity, damp);
+			transform.Rotate(0, 0, _rotationAmount);
 		}
+
+
 		if(Input.GetKey("d")){
 			//roll ship right by rotateAmount each frame
-			transform.Rotate(0,0, -rotateAmount);
+			//transform.Rotate(0,0, -rotateAmount);
+			_rotationAmount = Mathf.SmoothDamp(_rotationAmount, -rotateAmount, ref _rotationAmountVelocity, damp);
+			transform.Rotate(0,0, _rotationAmount);
+		} else {
+			_rotationAmount = Mathf.SmoothDamp(_rotationAmount, 0, ref _rotationAmountVelocity, damp);
+			transform.Rotate(0,0, _rotationAmount);
 		}
 
 		//Flight Stick controls
@@ -68,6 +111,23 @@ public class FlySimple : MonoBehaviour {
 		if(Input.GetKey("d")){
 			//roll ship right by rotateAmount each frame
 			transform.Rotate(0,0, -rotateAmount);
+		}
+
+		//set speed
+		if(Input.GetKey("0")){
+			speed = 0;
+		}
+		if(Input.GetKey("1")){
+			speed = 25;
+		}
+		if(Input.GetKey("2")){
+			speed = 50;
+		}
+		if(Input.GetKey("3")){
+			speed = 75;
+		}
+		if(Input.GetKey("4")){
+			speed = 100;
 		}
 
 
